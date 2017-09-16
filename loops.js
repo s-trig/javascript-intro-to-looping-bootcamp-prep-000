be756740b06b5250d1b37cebd7df18753d066e34
David Reep <davereep@gmail.com>
2017-09-16 21:05:04 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+
+function forLoop(array) {
+  for(i = 1;i <= 25;i++) {
+    if (i === 1) {
+      array.push(`I am 1 strange loop.`)
+    }
+    else
+    array.push(`I am ${i} strange loops.`)
+  }
+  return array
+}
+
+function whileLoop(num) {
+  let countdown = num
+
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+}
